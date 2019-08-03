4f87cb5de500105656598053f8fc3a3f3dcbeb8f
nanorocks <andrejnankov@gmail.com>
2019-08-03 11:46:48 +0200

    ADD new validation for history endpoint and implementing new route

--- api/src/classes/ExchangeRate.php
+++ api/src/classes/ExchangeRate.php
@@ -1,11 +1,16 @@
 <?php
 
 
-class ExchangeRate extends ValidateExchangeRate {
+class ExchangeRate extends ValidateExchangeRate
+{
 
     public static $url;
 
     public static $apiEndpoints;
+
+    public static $customUrl;
+
+    public static $token;
 
     /**
      * On load static content
@@ -17,21 +22,40 @@ class ExchangeRate extends ValidateExchangeRate {
     }
 
     /**
-     *
+     * List add exchange rats
      */
     public static function list()
     {
+
         $url = self::$url;
-        $jsonResponse = file_get_contents($url);
-        $response = json_decode($jsonResponse,true);
+        try {
+
+            $jsonResponse = file_get_contents($url);
+            $response = json_decode($jsonResponse, true);
+
+        } catch (Exception $e) {
+            $response = [
+                'error' => true,
+                'status_text' => 'Internal server error',
+                'status_code' => 500,
+                'data' => null
+            ];
+        }
+
         echo Flight::json($response);
         die();
     }
 
 
+    /**
+     * Convert price from from - to
+     *
+     * @param $from
+     * @param $to
+     * @param $price
+     */
     public static function converter($from, $to, $price)
     {
-
         $response = [
             'error' => true,
             'status_text' => 'Invalid parameters',
@@ -40,8 +64,7 @@ class ExchangeRate extends ValidateExchangeRate {
         ];
 
         $validator = self::validateConverter($to, $from, $price);
-        if($validator)
-        {
+        if ($validator) {
             echo Flight::json($response);
             die();
         }
@@ -50,17 +73,29 @@ class ExchangeRate extends ValidateExchangeRate {
         $to = strtoupper($to);
 
         $url = self::$url;
-        $jsonResponse = file_get_contents($url);
-        $rates = json_decode($jsonResponse,  JSON_UNESCAPED_UNICODE);
+        try {
 
-        foreach ($rates['data'] as $rate)
-        {
-            if($rate['oznaka'] == $from)
-            {
+            $jsonResponse = file_get_contents($url);
+            $rates = json_decode($jsonResponse, JSON_UNESCAPED_UNICODE);
+
+        } catch (Exception $e) {
+
+            $response = [
+                'error' => true,
+                'status_text' => 'Internal server error',
+                'status_code' => 500,
+                'data' => null
+            ];
+
+            echo Flight::json($response);
+            die();
+        }
+
+        foreach ($rates['data'] as $rate) {
+            if ($rate['oznaka'] == $from) {
                 $from = $rate;
             }
-            if($rate['oznaka'] == $to)
-            {
+            if ($rate['oznaka'] == $to) {
                 $to = $rate;
             }
         }
@@ -68,26 +103,21 @@ class ExchangeRate extends ValidateExchangeRate {
         $denar = 0;
         $finalPrice = 0;
         // FOR DENAR-VALUE ONLY
-        if(is_string($to) && strtoupper($to) == 'MKD')
-        {
+        if (is_string($to) && strtoupper($to) == 'MKD') {
             $finalPrice = (floatval($price) * floatval($from['sreden']));
             $denar = 1;
-        }
-        else if(is_string($from) && strtoupper($from) == 'MKD')
-        {
+        } else if (is_string($from) && strtoupper($from) == 'MKD') {
             $finalPrice = (floatval($price) / floatval($to['sreden']));
             $denar = 1;
         }
 
-        if (!$denar)
-        {
-            if(!is_array($to) || !is_array($from))
-            {
+        if (!$denar) {
+            if (!is_array($to) || !is_array($from)) {
                 echo Flight::json($response);
                 die();
             }
 
-            $finalPrice = ( floatval($price) * floatval($from['sreden']) ) / floatval($to['sreden']);
+            $finalPrice = (floatval($price) * floatval($from['sreden'])) / floatval($to['sreden']);
         }
 
         $response['error'] = false;
@@ -101,10 +131,111 @@ class ExchangeRate extends ValidateExchangeRate {
         die();
     }
 
-
+    /**
+     * History of 15 days behind
+     *
+     * @param $value
+     */
     public static function history($value)
     {
-        echo 'hello world!' . getenv('MAIN_API_PATH');
+        $url = self::$url;
+        try {
+
+            $jsonResponse = file_get_contents($url);
+            $rates = json_decode($jsonResponse, JSON_UNESCAPED_UNICODE);
+
+        } catch (Exception $e) {
+
+            $response = [
+                'error' => true,
+                'status_text' => 'Internal server error',
+                'status_code' => 500,
+                'data' => null
+            ];
+
+            echo Flight::json($response);
+            die();
+        }
+
+        $validator = self::validateCurrencyValue($value, $rates['data']);
+
+        if ($validator) {
+            $response = [
+                'error' => true,
+                'status_text' => 'Invalid parameters',
+                'status_code' => 200,
+                'data' => null
+            ];
+
+            echo Flight::json($response);
+            die();
+        }
+
+        $value = strtoupper($value);
+
+        // last 15 days
+        $dateNow = date('Y-m-d');
+
+        // Implement caching if date not change
+        Flight::lastModified($dateNow);
+
+        $responseData = [];
+
+        for ($i = 0; $i < 15; $i++) {
+            $dateBehind = date('Y-m-d', strtotime($dateNow . ' -' . $i . ' day'));
+            $customUrl = self::$customUrl . $dateBehind . '?token=' . self::$token;
+
+            try {
+
+                $jsonResponse = file_get_contents($customUrl);
+                $rates = json_decode($jsonResponse, JSON_UNESCAPED_UNICODE);
+                $data = $rates['data'];
+                $statusCode = $rates['status_code'];
+
+            } catch (Exception $e) {
+
+                $response = [
+                    'error' => true,
+                    'status_text' => 'Internal server error',
+                    'status_code' => 500,
+                    'data' => null
+                ];
+
+                echo Flight::json($response);
+                die();
+            }
+
+            if ($statusCode != 200) {
+                $response = [
+                    'error' => true,
+                    'status_text' => 'Service Unavailable',
+                    'status_code' => 503,
+                    'data' => null
+                ];
+
+                echo Flight::json($response);
+                die();
+            }
+
+            // status code == 200
+            foreach ($data as $rate) {
+
+                if ($rate['oznaka'] == $value) {
+                    $responseData[] = [
+                        'datum' => $rate['datum'],
+                        'kupoven' => $rate['kupoven'],
+                        'sreden' => $rate['sreden'],
+                        'prodazen' => $rate['prodazen'],
+                        'oznaka' => $rate['oznaka']
+                    ];
+                    break;
+                }
+            }
+
+        }
+
+        echo Flight::json($responseData);
+        die();
     }
 
 }
