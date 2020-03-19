e5d0a14c6eeec5546fc038989f1ea2e8625492b6
nanorocks <andrejnankov@gmail.com>
2020-03-19 14:30:39 +0100

    ADD accesskey middleware and add facades

--- api/app/Http/Middleware/KeyAccessMiddleware.php
+++ api/app/Http/Middleware/KeyAccessMiddleware.php
@@ -17,13 +17,13 @@ class KeyAccessMiddleware
     public function handle($request, Closure $next)
     {
         $keyAccess = $request->get('keyAccess');
-        // if (empty($keyAccess) ||  $keyAccess != env('APP_KEY'))
-        // {
-        //     return response()->json([
-        //         'status' => 'Invalid access',
-        //         'code' => 401
-        //     ], 401);
-        // }
+         if (empty($keyAccess) ||  $keyAccess != env('APP_KEY'))
+         {
+             return response()->json([
+                 'status' => 'Invalid access',
+                 'code' => 401
+             ], 401);
+         }
 
         return $next($request);
     }
