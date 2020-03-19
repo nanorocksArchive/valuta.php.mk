423f19b9bac34d669e5b2f28e7c0fb1ae95d1c0c
nanorocks <andrejnankov@gmail.com>
2020-03-19 15:42:33 +0100

    RM beckup

--- beckup/api/src/route/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Flight::route('GET /', ['LoadClass', 'onload']);
-
-Flight::route('POST /api/list', ['ListClass', 'list']);
-
-Flight::route('POST /api/converter/@from/@to/@price', ['ConverterClass', 'converter']);
-
-Flight::route('POST /api/history/@value', ['HistoryClass', 'history']);
--- beckup/api/src/template/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo "<pre>" . $endpoints . "</pre>";
